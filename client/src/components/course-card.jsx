ed6a6fd08066bf5b866551c2703ea5bdb87fbdeb
nickerick <nickerickson737@gmail.com>
2023-12-22 22:26:50 -0600

    further flesh out react folder structure

---
+++ client/src/components/course-card.jsx
@@ -0,0 +1,7 @@
+import { Box } from '@mui/material';
+
+import './course-card.css';
+
+export default function CourseCard() {
+  return <Box id="course-card-container">I'm a course tbh</Box>;
+}
--- client/src/pages/marketplace/Marketplace.jsx
+++ client/src/pages/marketplace/Marketplace.jsx
@@ -1,7 +1,12 @@
+import CourseCard from '../../components/course-card';
+
 export default function Marketplace() {
   return (
     <div>
       <p>This is the marketplace &#128526;</p>
+      <CourseCard />
+      <CourseCard />
+      <CourseCard />
     </div>
   );
 }
