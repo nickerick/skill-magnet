bf7d7bc05a0afa878297e34e43e6a1e054f98154
notbrayton <brayton.rude@gmail.com>
2024-02-20 13:38:57 -0600

    added findUserById and some comments

--- server/src/main/java/com/skillmagnet/User/UserController.java
+++ server/src/main/java/com/skillmagnet/User/UserController.java
@@ -14,7 +14,15 @@ public class UserController  {
     @Autowired
     UserRepository userRepository;
 
-    @PostMapping("/user")
+    /**
+     * Creates a new user out of the given username and password.
+     * @param username - string containing username
+     * @param password - string containing plaintext password
+     * @return
+     *          Success - The Created User
+     *          Failure - BAD_REQUEST (Username Taken)
+     */
+    @PostMapping("/user/create")
     public ResponseEntity<User> createUser(@RequestParam("username") String username,
                                            @RequestParam("password") String password) {
         // Username taken check
@@ -23,15 +31,22 @@ public class UserController  {
 
             userRepository.save(newUser);
 
-            return new ResponseEntity<>(newUser, HttpStatus.OK);
+            return new ResponseEntity<>(newUser, HttpStatus.BAD_REQUEST);
         }
 
         return new ResponseEntity<>(null, HttpStatus.OK);
     }
 
-    @GetMapping("/user")
-    public ResponseEntity<User> getUser(@RequestParam("username") String username) {
-        User user = userRepository.findByUsername(username);
+    /**
+     * Retrieve a user by id.
+     * @param id - user's unique int id
+     * @return
+     *          Success: Requested User
+     *          Failure: NOT_FOUND
+     */
+    @GetMapping("/user/{id}")
+    public ResponseEntity<User> getUser(@PathVariable("id") int id) {
+        User user = userRepository.findById(id);
 
         if (user == null) {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
@@ -40,8 +55,17 @@ public class UserController  {
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
-    @PutMapping("/login")
-    public ResponseEntity<String> loginCheck(@RequestParam("username") String username,
+    /**
+     * Compares login credentials to stored credentials.
+     * @param username - name of user attempting login
+     * @param password - password provided at login (Plaintext)
+     * @return
+     *         Success(Match): "Passwords Match!"
+     *         Success(Mismatch): "Passwords do not match!"
+     *         Failure: NOT_FOUND
+     */
+    @PutMapping("/user/login")
+    public ResponseEntity<String> loginUser(@RequestParam("username") String username,
                                               @RequestParam("password") String password) {
         User user = userRepository.findByUsername(username);
         if (user == null) {
