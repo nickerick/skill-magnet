ca6cab7892f53d8a75bae6b4d38696784f284217
Sam DeFrancisco <sjdefran@gmail.com>
2024-02-20 23:15:48 -0600

    small start, want to try something quick

--- server/src/main/java/com/skillmagnet/Enrolls/Enrolls.java
+++ server/src/main/java/com/skillmagnet/Enrolls/Enrolls.java
@@ -4,13 +4,37 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.skillmagnet.Course.Course;
+import com.skillmagnet.User.User;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import lombok.Data;
 
-// @Entity
-// @Table(name = "enrolls")
-// public class Enrolls {
+@Data
+@Entity
+@Table(name = "enrolls")
+public class Enrolls {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @CreationTimestamp
+    private LocalDateTime enrolledAt;
+    private int progress;
+
+    @OneToOne
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private Course enrolledCourse;
+    //@JoinTable
+    private User enrolledUser;
     
-// }
+
+    public Enrolls() {}
+
+    // public Enrolls()
+}
