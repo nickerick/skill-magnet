f7bed2cf552e982b0c7314f9da82c30db3054314
Sam DeFrancisco <sjdefran@gmail.com>
2024-02-28 23:02:19 -0600

    commit before changes

--- server/src/main/java/com/skillmagnet/Enrolls/EnrollsController.java
+++ server/src/main/java/com/skillmagnet/Enrolls/EnrollsController.java
@@ -88,6 +88,7 @@ public class EnrollsController {
         // TODO: Create Actual progress system
         // Once lessons are done maybe something like
         // (lessonsCompleted / lessonsAvailable) * 100
+        enrolledCourse.getLessons();
         enrollment.setProgress(100);
         enrollsRepository.save(enrollment);
         return new ResponseEntity<>(enrollment, HttpStatus.OK);
