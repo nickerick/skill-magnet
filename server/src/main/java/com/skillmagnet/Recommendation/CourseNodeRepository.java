e3bdcbde2d025ad2426cf38a37f5250a629d5073
nickerick <nickerickson737@gmail.com>
2024-03-31 12:29:01 -0500

    fix query

--- server/src/main/java/com/skillmagnet/Recommendation/CourseNodeRepository.java
+++ server/src/main/java/com/skillmagnet/Recommendation/CourseNodeRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface CourseNodeRepository extends Neo4jRepository<CourseNode, Integer> {
     @Query("MATCH (u:User)-[:ENROLLED_IN]->(c:Course)<-[:ENROLLED_IN]-(other:User)-[:ENROLLED_IN]->(otherCourse:Course) " +
-            "WHERE u.user_id = $userId " +
+            "WHERE u.userId = $userId " +
             "AND NOT (u)-[:ENROLLED_IN]->(otherCourse) " +
             "AND otherCourse <> c " +
             "WITH otherCourse, COUNT(DISTINCT other) AS popularity " +
