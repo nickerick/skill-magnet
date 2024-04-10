6430b0707e21fc9329a506348d906fd4c9bbfce2
Naga Vempati <nvempati@iastate.edu>
2024-04-09 19:19:46 -0500

    made addding lesson functionality

---
+++ client/src/components/courseinformation/BasicLessonInformation.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './BasicLessonInformation.css';
+import SmallHeader from '../SmallHeader';
+
+const BasicLessonInformation = () => {
+  const [title, setTitle] = useState('');
+
+  const handleTitleChange = (e) => {
+      setTitle(e.target.value);
+  };
+
+  return (
+        <div className="lesson-form">
+            <div className="left-group">
+                <div className="input-group">
+                    <SmallHeader headerName={"Lesson Title:"} />
+                    <input
+                        type="text"
+                        id="lessonTitle"
+                        value={title}
+                        onChange={handleTitleChange}
+                    />
+                </div>
+
+                <div className="input-group">
+                    <SmallHeader headerName={"Lesson Video:"} />
+                    <div className="upload-container">
+                      <label htmlFor="lessonVideoFile" className="upload-button">Select File</label>
+                      <input
+                        type="file"
+                        id="courseThumbnail"
+                        className="file-input"
+                      />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BasicLessonInformation;
--- client/src/pages/createnewcourse/CreateNewCourse.jsx
+++ client/src/pages/createnewcourse/CreateNewCourse.jsx
@@ -1,10 +1,32 @@
+import React, { useState } from "react";
 import Header from "../../components/Header";
 import BasicCourseInformation from "../../components/courseinformation/BasicCourseInformation";
+import BasicLessonInformation from "../../components/courseinformation/BasicLessonInformation";
+import plusIcon from "../../assets/math-plus-icon.svg";
+import "./CreateNewCourse.css"; 
+
 export default function CreateNewCourse() {
-    return(
+    const [lessons, setLessons] = useState([<BasicLessonInformation key={0} />]);
+
+    const addLesson = () => {
+        setLessons([...lessons, <BasicLessonInformation key={lessons.length} />]);
+    };
+
+    return (
         <div className="create-course">
-            <div ClassName="page-title"><Header headerName="Course Information" /></div>
+            <div className="page-title">
+                <Header headerName="Course Information" />
+            </div>
             <BasicCourseInformation />
+            <div className="page-title add-lessons-header">
+                <Header headerName="Add Lessons" />
+                <button className="add-lesson-button" onClick={addLesson}>
+                    <img src={plusIcon} className="plus-sign" alt="plus sign" />
+                </button>
+            </div>
+            {lessons.map((lesson, index) => (
+                <div key={index}>{lesson}</div>
+            ))}
         </div>
-    ); 
+    );
 }
