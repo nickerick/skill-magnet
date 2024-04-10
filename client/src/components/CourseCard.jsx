942636c547aefeb9e6d529ce065cd8d007d691e0
Jennifer Robles <jenn26@iastate.edu>
2024-04-10 02:29:48 -0500

    adding enrollservice

--- client/src/components/CourseCard.jsx
+++ client/src/components/CourseCard.jsx
@@ -1,7 +1,39 @@
-
 import './CourseCard.css';
+import { useState } from 'react';
+import EnrollsService from '../services/EnrollsService.js';
 
-export default function CourseCard({courseTitle, imageUrl, instructorName}) {
+export default function CourseCard({ courseId, courseTitle, imageUrl, description }) {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleEnrollClick = () => {
+    setShowPopup(true);
+  };
+
+  const handleEnrollConfirm = async () => {
+    setShowPopup(false);
+    try {
+      const isEnrolled = await EnrollsService.checkEnrollment(courseId, 1);
+      if (!isEnrolled) {
+        const user_course = {
+          courseId: courseId,
+          userId: 1 //hardcoded user id
+        };
+        console.log(user_course);
+        await EnrollsService.enrollUser(user_course);
+        window.alert('Successfully enrolled in course.');
+      }
+      else {
+        window.alert('You are already enrolled in this course.');
+      }
+    } catch (error) {
+      alert('ERROR: Failed to enroll user');
+    }
+  };
+
+  const handleEnrollCancel = () => {
+    setShowPopup(false);
+  };
+
   return (
     <div className="course-card">
       <div className="course-image">
@@ -10,9 +42,22 @@ export default function CourseCard({courseTitle, imageUrl, instructorName}) {
       <div className="course-title">
         {courseTitle}
       </div>
-      <div className="course-instructor">
-        {instructorName}
+      <div className="course-description">
+        {description}
       </div>
-      <button className="enroll-button">Enroll Now</button>
-</div>
-  );}
+      <button className="enroll-button" onClick={handleEnrollClick}>Enroll Now</button>
+
+      {showPopup && (
+        <div className="popup-background">
+          <div className="popup-content">
+            <p>Enroll in {courseTitle}?</p>
+            <div>
+              <button onClick={handleEnrollConfirm}>Yes</button>
+              <button onClick={handleEnrollCancel}>No</button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
