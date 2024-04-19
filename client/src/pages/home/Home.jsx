2aff2bac629529a70eb2f3a853b9b9ad465bcada
Jennifer Robles <jennyrobles08@live.com>
2024-04-19 16:37:16 -0500

    adding info to homepage + hover effect on my course card (#55)
    * adding info to homepage
    
    * spacing
    
    * making image stretch to entire page
    
    * hardfix
    
    * fixing horizontal bar appearing
    
    * hover effect on enrolled courses
    
    ---------
    
    Co-authored-by: Jennifer Robles <jenn26@iastate.edu>

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,19 +1,24 @@
-import { useState } from 'react';
-
-import { Box, Button } from '@mui/material';
-import reactLogo from '/react.svg';
-import viteLogo from '/vite.svg';
-
+import { useEffect, useState } from 'react';
 import './Home.css';
 import cover from '../../assets/site_background.jpg';
+import enroll_courses_icon from '../../assets/enroll-courses-icon.png';
+import notepad_icon from '../../assets/notepad-icon.png';
+import take_quizzes_icon from '../../assets/take-quizzes-icon.png';
 
 export default function Home() {
   const [count, setCount] = useState(0);
 
+  useEffect(() => {
+    document.body.style.overflowX = 'hidden';
+    return () => {
+      document.body.style.overflowX = 'auto';
+    };
+  }, []);
+
   return (
     <>
       <div className="homepage-photo">
-        <img src={cover} />
+        <img src={cover} style={{width: '100vw'}}/>
         <div className="text-card">
           <h1>One stop shop for your learning needs</h1>
           <h2>Educational content and study tools in one place</h2>
@@ -34,8 +39,37 @@ export default function Home() {
         </h3>
       </div>
 
-      <div className="quotes">
-
+      <div className="feature-section">
+        <div className="feature-container">
+          <div className="feature">
+            <img className="feature-icon"
+                 src={enroll_courses_icon}
+                 alt="Enroll in Courses Icon"
+                 height={40}/>
+            <h3 className="feature-title">Discover and Enroll in Courses</h3>
+            <p className="feature-description">Explore our selection of courses and topics. Users can easily browse through
+              our course marketplace and enroll in courses that align with their interests and learning objectives. With just a few
+              clicks, embark on your educational journey and start learning at your own pace.</p>
+          </div>
+          <div className="feature">
+            <img className="feature-icon"
+                 src={notepad_icon}
+                 alt="Take Notes Icon" />
+            <h3 className="feature-title">Take Notes</h3>
+            <p className="feature-description">Utilize our integrated notepad feature to jot down important insights, key takeaways,
+              and personal reflections as you progress through each course. Keep your study materials organized and easily accessible,
+              ensuring that you can review and reinforce your learning whenever necessary.</p>
+          </div>
+          <div className="feature">
+            <img className="feature-icon"
+                 src={take_quizzes_icon}
+                 alt="Assess Your Knowledge Icon" />
+            <h3 className="feature-title">Assess Your Knowledge</h3>
+            <p className="feature-description"> Challenge yourself and reinforce your learning with our dynamic quizzes tailored to
+              each course. Identify areas for improvement and solidify your understanding, whether you're preparing for exams or
+              simply reinforcing your knowledge.</p>
+          </div>
+        </div>
       </div>
 
     </>
