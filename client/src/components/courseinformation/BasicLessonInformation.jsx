b75165a3718f4d8a1ec2c2e972e3a8d0cec9fc03
Jennifer Robles <jenn26@iastate.edu>
2024-04-23 23:04:44 -0500

    fixing mapping

--- client/src/components/courseinformation/BasicLessonInformation.jsx
+++ client/src/components/courseinformation/BasicLessonInformation.jsx
@@ -2,40 +2,56 @@ import React, { useState } from 'react';
 import './BasicLessonInformation.css';
 import SmallHeader from '../SmallHeader';
 
-const BasicLessonInformation = () => {
+const BasicLessonInformation = ({ lessonId, handleLessonInfoChange, handleFileInputChange }) => {
   const [title, setTitle] = useState('');
+  const [file, setFile] = useState(null);
+  const [fileName, setFileName] = useState('');
 
   const handleTitleChange = (e) => {
-      setTitle(e.target.value);
+    setTitle(e.target.value);
+    handleLessonInfoChange(lessonId, { title: e.target.value });
+  };
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    if (selectedFile) {
+      setFile(selectedFile);
+      setFileName(selectedFile.name);
+      handleFileInputChange(e, lessonId);
+    }
   };
 
   return (
-        <div className="lesson-form">
-            <div className="left-group">
-                <div className="input-group">
-                    <SmallHeader headerName={"Lesson Title:"} />
-                    <input
-                        type="text"
-                        id="lessonTitle"
-                        value={title}
-                        onChange={handleTitleChange}
-                    />
-                </div>
-
-                <div className="input-group">
-                    <SmallHeader headerName={"Lesson Video:"} />
-                    <div className="upload-container">
-                      <label htmlFor="courseThumbnail" className="upload-button">Choose Image</label>
-                      <input
-                        type="file"
-                        id="courseThumbnail"
-                        className="file-input"
-                      />
-                  </div>
-                </div>
-            </div>
+    <div className="lesson-form">
+      <div className="left-group">
+        <div className="input-group">
+          <SmallHeader headerName={"Lesson Title:"} />
+          <input
+            type="text"
+            id={`lessonTitle${lessonId}`}
+            value={title}
+            onChange={(e) => handleTitleChange(e)}
+            className="lesson-title-input"
+          />
         </div>
-    );
+
+        <div className="input-group">
+          <SmallHeader headerName={'Lesson Video:'} />
+          <div className="upload-container">
+            <label htmlFor={`lessonVideo${lessonId}`} className="lesson-upload-button">
+              {fileName ? fileName : 'Choose File'}
+            </label>
+            <input
+              type="file"
+              id={`lessonVideo${lessonId}`}
+              className="file-input"
+              onChange={handleFileChange}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BasicLessonInformation;
--- client/src/pages/createnewcourse/CreateNewCourse.jsx
+++ client/src/pages/createnewcourse/CreateNewCourse.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import Header from "../../components/Header";
 import BasicCourseInformation from "../../components/courseinformation/BasicCourseInformation";
 import BasicLessonInformation from "../../components/courseinformation/BasicLessonInformation";
@@ -6,33 +6,116 @@ import plusIcon from "../../assets/math-plus-icon.svg";
 import "./CreateNewCourse.css";
 
 export default function CreateNewCourse() {
-    const [lessons, setLessons] = useState([<BasicLessonInformation key={0} />]);
+    const [courseInfo, setCourseInfo] = useState({
+        lessons: [],
+        title: '',
+        description: '',
+        category: '',
+    });
+
+    const [lessons, setLessons] = useState([{ id: 0, title: '', selectedFile: null, videoLink: '' }]);
 
     const addLesson = () => {
-        setLessons([...lessons, <BasicLessonInformation key={lessons.length} />]);
+        const newId = lessons.length;
+        setLessons([...lessons, {
+            id: newId,
+            title: '',
+            selectedFile: null,
+            videoLink: '' }]);
     };
 
+    const handleLessonInfoChange = (lessonId, lessonInfo) => {
+        setLessons(prevLessons => {
+            const updatedLessons = prevLessons.map(lesson => {
+                if (lesson.id === lessonId) {
+                    return { ...lesson, ...lessonInfo };
+                }
+                return lesson;
+            });
+            return updatedLessons;
+        });
+    };
+
+    const handleFileInputChange = (e, lessonId) => {
+        const file = e.target.files[0];
+        const newLessons = lessons.map(lesson => {
+            if (lesson.id === lessonId) {
+                return {
+                    ...lesson,
+                    selectedFile: file,
+                    videoLink: file ? file.name : ''
+                };
+            }
+            return lesson;
+        });
+        setLessons(newLessons);
+    };
+
+    const handleTitleChange = (e, lessonId) => {
+        const newTitle = e.target.value;
+        const newLessons = lessons.map(lesson => {
+            if (lesson.id === lessonId) {
+                return {
+                    ...lesson,
+                    title: newTitle
+                };
+            }
+            return lesson;
+        });
+        setLessons(newLessons);
+    };
+
+
+
+    const handleCreateCourse = () => {
+       //testing
+        //const lessonTitles = lessons.map(lesson => lesson.title);
+        //const lessonFiles = lessons.map(lesson => lesson.selectedFile);
+
+        const updatedCourseInfo = {
+            ...courseInfo,
+            lessons: lessons.map(lesson => ({
+                title: lesson.title,
+                videoLink: lesson.videoLink
+            }))
+        };
+
+        console.log(updatedCourseInfo);
+        // console.log('Lesson Titles:', lessonTitles);
+        // console.log('Lesson Video Files:', lessonFiles);
+    };
+
+
     return (
-        <div>
-            <div className="create-course">
-                <div className="page-title">
-                    <Header headerName="Course Information" />
+      <div>
+          <div className="create-course">
+              <div className="page-title">
+                  <Header headerName="Course Information" />
+              </div>
+              <BasicCourseInformation
+                setCourseInfo={setCourseInfo}
+                courseInfo={courseInfo}
+              />
+              <div className="page-title add-lessons-header">
+                  <Header headerName="Add Lessons" />
+                  <button className="add-lesson-button" onClick={addLesson}>
+                      <img src={plusIcon} className="plus-sign" alt="plus sign" />
+                  </button>
+              </div>
+              {lessons.map((lesson, index) => (
+                <div key={index}>
+                    <BasicLessonInformation
+                      lessonId={lesson.id}
+                      handleLessonInfoChange={handleLessonInfoChange}
+                      handleFileInputChange={handleFileInputChange}
+                    />
+
                 </div>
-                <BasicCourseInformation />
-                <div className="page-title add-lessons-header">
-                    <Header headerName="Add Lessons" />
-                    <button className="add-lesson-button" onClick={addLesson}>
-                        <img src={plusIcon} className="plus-sign" alt="plus sign" />
-                    </button>
-                </div>
-                {lessons.map((lesson, index) => (
-                    <div key={index}>{lesson}</div>
-                ))}
-            </div>
-            <div className="create-course-button-container">
-                <button className="create-course-button">Create Course</button>
-            </div>
-        </div>
+              ))}
+          </div>
+          <div className="create-course-button-container">
+              <button className="create-course-button" onClick={handleCreateCourse}>Create Course</button>
+          </div>
+      </div>
     );
 }
-
